3cf03b0fe58e69442572eef4e71c6b3e46d4fd6f
arturo-source <arturojuan9898@gmail.com>
2023-11-26 17:55:31 +0100

    Hash all files in the folder Code extracted from https://go.dev/blog/pipelines

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
-func todo(directory string) (string, error) {
-	// todo
-	return "", nil
+func MD5All(directory string) (map[string][md5.Size]byte, error) {
+	m := make(map[string][md5.Size]byte)
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		m[path] = md5.Sum(data)
+		return nil
+
+	})
+
+	return m, err
 }
 
 func run() error {
@@ -24,7 +45,7 @@ func run() error {
 		return errors.New("directory must be provided using -d option")
 	}
 
-	result, err := todo(directory)
+	hashes, err := MD5All(directory)
 	if err != nil {
 		return err
 	}
@@ -40,8 +61,11 @@ func run() error {
 		out = f
 	}
 
-	_, err = out.Write([]byte(result))
-	return err
+	for k, v := range hashes {
+		fmt.Fprintf(out, "%s %x\n", k, v)
+	}
+
+	return nil
 }
 
 func main() {
